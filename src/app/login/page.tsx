493ddec2fd753aa424bf2773a805acd662e71c71
Divyesh Patel <dev.eth007@gmail.com>
2024-04-02 01:18:21 +0530

    Add routes for login and register and configure shadcn

---
+++ src/app/login/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
+
+function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen  ">
+      <form className="border border-slate-200 flex flex-col items-center h-[300px] p-4 rounded-md shadow-md">
+        <h1 className="text-2xl font-semibold">Login</h1>
+        <div className="mt-10">
+          <Input type="email" id="email" placeholder="Email" />
+        </div>
+        <div className="mt-5">
+          <Input type="password" id="password" placeholder="Password" />
+        </div>
+        <Button className="w-full mt-5">Login</Button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
